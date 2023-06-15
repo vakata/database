639fe9ca9a4fd75e5748fe7460d410b5ac724893
Ivan Bozhanov <git@vakata.com>
2023-06-15 12:05:39 +0300

    fixed oracle filter

--- src/schema/TableQuery.php
+++ src/schema/TableQuery.php
@@ -349,7 +349,7 @@ class TableQuery implements \IteratorAggregate, \ArrayAccess, \Countable
                 $par[] = array_slice($value, $i, 500);
             }
             $sql = '(' . implode($negate ? ' AND ' : ' OR ', $sql) . ')';
-            return [ $sql, [$par] ];
+            return [ $sql, $par ];
         }
         return [
             $negate ? $name . ' NOT IN (??)' : $name . ' IN (??)',
